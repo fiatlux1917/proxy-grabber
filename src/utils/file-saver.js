ded37b1b35e7a4c08b707eb134069256084c2890
JubasNTC <31780672+JubasNTC@users.noreply.github.com>
2019-10-03 16:04:39 +0300

    updated countries list

--- src/utils/file-saver.js
+++ src/utils/file-saver.js
@@ -4,8 +4,8 @@ const getSaveFormat = proxies => {
   return proxies.map(proxy => `${proxy.ip}:${proxy.port}`)
 }
 
-export const saveProxy = proxies => {
+export const saveProxy = (proxies, saveTo = 'proxy.txt') => {
   const result = getSaveFormat(proxies).join('\n')
   const data = new Blob([result], { type: 'text/plain' })
-  saveAs(data, 'proxy.txt')
+  saveAs(data, saveTo)
 }
