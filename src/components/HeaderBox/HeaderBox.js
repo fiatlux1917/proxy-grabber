09d2ba2804405d0e1fb334224a4422d1e69ea66e
JubasNTC <31780672+JubasNTC@users.noreply.github.com>
2019-10-06 22:49:51 +0300

    update header label

--- src/components/HeaderBox/HeaderBox.js
+++ src/components/HeaderBox/HeaderBox.js
@@ -5,7 +5,7 @@ import { URL_GITHUB_PROFILE, URL_BHF_TOPIC } from '@/constants/grabber'
 const HeaderBox = () => {
   return (
     <h4>
-      Proxy grabber | by <a href={URL_GITHUB_PROFILE}>JubasNTC</a>|
+      Proxy grabber | by <a href={URL_GITHUB_PROFILE}>JubasNTC</a>{' '}
       <a href={URL_BHF_TOPIC}>[BHF]</a>
     </h4>
   )
